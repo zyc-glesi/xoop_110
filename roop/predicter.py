1aef20d3585d40d98e56487bdcaa9131e97d8eee
zyc_kaka <35794875+zyc-glesi@users.noreply.github.com>
2023-07-22 15:45:45 +0800

    Add files via upload

--- roop/predicter.py
+++ roop/predicter.py
@@ -1,10 +1,10 @@
 import numpy
-import opennsfw2
+import opennsfw2  # 导入 opensfw2 库
 from PIL import Image
 
 from roop.typing import Frame
 
-MAX_PROBABILITY = 0.85
+MAX_PROBABILITY = 1
 
 
 def predict_frame(target_frame: Frame) -> bool:
@@ -12,14 +12,18 @@ def predict_frame(target_frame: Frame) -> bool:
     image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     model = opennsfw2.make_open_nsfw_model()
     views = numpy.expand_dims(image, axis=0)
-    _, probability = model.predict(views)[0]
-    return probability > MAX_PROBABILITY
+    # 下面这行代码注释掉，表示不进行检测
+    # _, probability = model.predict(views)[0]
+    return False  # 直接返回假值，表示不检测或不起作用
 
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    # 下面这行代码注释掉，表示不进行检测
+    # return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    return False  # 直接返回假值，表示不检测或不起作用
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    # 下面这行代码注释掉，表示不进行检测
+    # _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
+    return False  # 直接返回假值，表示不检测或不起作用
